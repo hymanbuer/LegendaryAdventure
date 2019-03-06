7c707536b42eb601231f8e388a81345f1eaf629c
hymanbuer <505228589@qq.com>
2019-03-07 01:02:05 +0800

    refined

--- assets/scripts/game/BattleField.js
+++ assets/scripts/game/BattleField.js
@@ -11,16 +11,20 @@ cc.Class({
         redBarLeft: cc.Sprite,
         redBarRight: cc.Sprite,
         monsterAttackSign: cc.Sprite,
+        playerAttackSign: cc.Sprite,
     },
 
     onLoad () {
-        this._showPlayerMiss()
-            .then(() => this._showMonsterAttack(48));
     },
 
-    onClickAutoPlay () {
-        this._showPlayerMiss()
-            .then(() => this._showMonsterAttack(48));
+    onClickAutoPlay (sender) {
+        if (sender.isChecked) {
+            this._showPlayerNormalAttack(36)
+                .then(() => this._showMonsterAttack(48));
+        } else {
+            this._showPlayerMiss()
+                .then(() => this._showMonsterAttack(48));
+        }
     },
 
     _showPlayerMiss () {
@@ -50,8 +54,45 @@ cc.Class({
         });
     },
 
+    _showPlayerNormalAttack (num) {
+        const showAttackNumber = callback => {
+            this.playerAttackNumber.node.active = true;
+            this.playerAttackNumber.string = `:${num}`;
+            this.playerAttackNumber.node.y = 98;
+            this.playerAttackNumber.node.opacity = 255;
+            cc.tween(this.playerAttackNumber.node)
+                .show()
+                .to(20/30, {y: 200})
+                .delay(4/30)
+                .to(8/30, {opacity: 0})
+                .hide()
+                .call(callback)
+                .start();
+        };
+
+        this.playerAttackSign.node.active = true;
+        this.playerAttackSign.node.opacity = 255;
+        this.monster.node.color = cc.color(236, 57, 57);
+        cc.tween(this.monster.node)
+            .show()
+            .by(4/30, {x: -16})
+            .by(4/30, {x: 16})
+            .then(cc.tintTo(4/30, 255, 255, 255))
+            .start();
+        cc.tween(this.playerAttackSign.node)
+            .show()
+            .delay(8/30)
+            .to(4/30, {opacity: 0})
+            .hide()
+            .start();
+
+        return new Promise(resolve => {
+            showAttackNumber(resolve);
+        });
+    },
+
     _showMonsterAttack (num) {
-        const showMonsterAttackNumber = callback => {
+        const showAttackNumber = callback => {
             this.monsterAttackNumber.node.active = true;
             this.monsterAttackNumber.string = `:${num}`;
             this.monsterAttackNumber.node.y = 98;
@@ -90,7 +131,7 @@ cc.Class({
                 .call(() => {
                     this.node.emit('monster-attack', num);
                     showShakeEffect();
-                    showMonsterAttackNumber(resolve)
+                    showAttackNumber(resolve)
                 })
                 .start();
         });
