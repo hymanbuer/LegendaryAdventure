9d78e6f4af4d24ea945cef2d4d7fa6c82c7691e1
hymanbuer <505228589@qq.com>
2019-03-23 01:41:02 +0800

    refined

--- assets/scripts/game/events/EventTalk.js
+++ assets/scripts/game/events/EventTalk.js
@@ -2,23 +2,6 @@
 const TalkType = require('PanelTalk').TalkType;
 const Game = require('Game');
 
-class EventAward {
-    constructor (event) {
-        this.awardGid = Number.parseInt(event.TASKAWARD);
-        this.message = event.MESSAGE;
-    }
-
-    fire (trigger) {
-        return new Promise(resolve => {
-            trigger.node.destroy();
-            Game.bag.addItem(this.awardGid);
-
-            return Game.openPanel('get_item_dialog', this.awardGid, this.message)
-                .then(() => Game.onPanelClosed('get_item_dialog', () => resolve(true)));
-        });
-    }
-}
-
 cc.Class({
     extends: require('EventTrigger'),
 
